c65065b118db0db080de7105190a5aac2d8be7b6
Ian Kersz <kerszamaral@gmail.com>
2023-07-29 18:27:39 -0300

    Created app main

--- src/SpendWise.java
+++ src/SpendWise.java
@@ -1,5 +1,8 @@
+import SpendWise.MainMenu;
+
 public class SpendWise {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.run();
     }
 }
