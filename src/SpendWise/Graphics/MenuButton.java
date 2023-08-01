3479721abd8781cb8c011a6e70b75b3ebcf70c77
Ian Kersz <kerszamaral@gmail.com>
2023-08-01 15:37:17 -0300

    Restrucutred Menu button class

--- src/SpendWise/Graphics/MenuButton.java
+++ src/SpendWise/Graphics/MenuButton.java
@@ -2,45 +2,58 @@ package SpendWise.Graphics;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import java.awt.Color;
 
 public class MenuButton extends JButton {
     private String name;
-    private ImageIcon iconClosed;
-    private ImageIcon iconOpen;
+    private ImageIcon icon;
+    // private ImageIcon iconOpen;
+    private Color Background;
 
-    public MenuButton(String name) {
-
+    public MenuButton(String name, Color background) {
         this.name = name.toLowerCase();
-        iconOpen = new ImageIcon("src\\Images\\" + this.name + "IconHover.png");
-        iconClosed = new ImageIcon("src\\Images\\" + this.name + "Icon.png");
+        this.Background = background;
+        // iconOpen = new ImageIcon("src\\Images\\" + this.name + "IconHover.png");
+        icon = new ImageIcon("src\\Images\\" + this.name + "Icon.png");
         this.initialize();
     }
 
     private void initialize() {
         // Visual settings
-        this.setIcon(iconClosed);
+        this.setIcon(icon);
         this.setText("");
         this.setSize(50, 50);
         this.setBorderPainted(false);
         this.setFocusPainted(false);
-        this.setContentAreaFilled(false);
-        this.setOpaque(false);
-        this.setVisible(false);
+        this.setContentAreaFilled(true);
+        this.setVisible(true);
+        this.setBackground(Background);
 
         // Aligment settings
         this.setAlignmentX(JButton.LEFT_ALIGNMENT);
         this.setHorizontalAlignment(JButton.LEFT);
         this.setHorizontalTextPosition(JButton.LEFT);
         this.setVerticalTextPosition(JButton.CENTER);
-
     }
 
-    public void openMenu() {
-        this.setIcon(this.iconOpen);
+    @Override
+    public void setSelected(boolean b) {
+        super.setSelected(b);
+        if (b) {
+            this.openMenu();
+        } else {
+            this.closeMenu();
+        }
     }
 
-    public void closeMenu() {
-        this.setIcon(this.iconClosed);
+    private void openMenu() {
+        this.setText(this.name);
+        // this.setIcon(this.iconOpen);
+    }
+
+    private void closeMenu() {
+        this.setText("");
+        // this.setIcon(this.iconClosed);
     }
 
 }
