9f5987bf6f61dd238969e6b222563f612a19f49f
Ian Kersz <kerszamaral@gmail.com>
2023-08-01 15:37:44 -0300

    Fixes and improvements made to the main menu

--- src/SpendWise/MainMenu.java
+++ src/SpendWise/MainMenu.java
@@ -2,174 +2,156 @@ package SpendWise;
 
 import SpendWise.Graphics.Menus.*;
 import SpendWise.Graphics.*;
+import SpendWise.Utils.Contexts;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class MainMenu extends JFrame {
-    private int curContext;
-    private boolean loggedIn;
+    private final static String AppName = "SpendWise";
     private final static int ScreenWidth = 1920 / 2;
     private final static int ScreenHeight = 1080 / 2;
-    private final static String AppName = "SpendWise";
     private final static Color BackgroundColor = new Color(0, 177, 216);
 
-    private final static String[] buttonNames = { "Account", "Bill", "Charts", "Groups", "Expenses" };
+    private final static String[] buttonNames = { "Account", "Bill", "Charts", "Groups", "Expenses", "Logout" };
 
-    private PanelScreen[] screens;
-    private MenuButton[] buttons;
+    private Contexts currentContext;
+    private Screen[] screens;
+    private JButton[] buttons;
+    private Box centerLayout;
+    private Box sidePanel;
 
     public MainMenu() {
         this.initialize();
     }
 
+    public boolean run() {
+        this.createMenus();
+        this.logout(null);
+        this.getContentPane().add(centerLayout, BorderLayout.CENTER);
+
+        LoginMenu login = (LoginMenu) this.screens[Contexts.LOGIN.ordinal()];
+        login.getBtnLogin().addActionListener(e -> login(e));
+
+        this.createButtons();
+        this.getContentPane().add(sidePanel, BorderLayout.EAST);
+        this.refresh();
+
+        return true;
+    }
+
     private void initialize() {
-        this.setSize(ScreenWidth, ScreenHeight);
-        this.setVisible(true);
         this.setTitle(AppName);
+        this.setSize(ScreenWidth, ScreenHeight);
+        this.getContentPane().setBackground(BackgroundColor);
+
+        this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().setLayout(new BorderLayout());
-        this.curContext = 0;
-        this.loggedIn = false;
-        this.getContentPane().setBackground(BackgroundColor);
-    }
 
-    public void setCurContext(int curContext) {
-        this.curContext = curContext;
-    }
-
-    public int getCurContext() {
-        return curContext;
-    }
-
-    public void setLoggedIn(boolean loggedIn) {
-        this.loggedIn = loggedIn;
-    }
-
-    public boolean getLoggedIn() {
-        return loggedIn;
-    }
-
-    private void closeButtons() {
-        for (int i = 0; i < buttons.length; i++) {
-            if (buttons[i] != null && buttons[i].isVisible())
-                buttons[i].setVisible(false);
-        }
-        return;
-    }
-
-    private void openButtons() {
-        for (int i = 0; i < buttons.length; i++) {
-            if (buttons[i] != null && !buttons[i].isVisible())
-                buttons[i].setVisible(true);
-        }
-        return;
+        this.currentContext = Contexts.LOGIN;
+        this.centerLayout = Box.createVerticalBox();
+        this.sidePanel = Box.createVerticalBox();
     }
 
     private void refresh() {
         this.revalidate();
         this.repaint();
-        return;
     }
 
-    private void clearScreen() {
-        for (int i = 0; i < screens.length; i++) {
-            screens[i].closeScreen();
-        }
-        return;
+    private void createMenus() {
+        screens = new Screen[Contexts.values().length];
+        screens[Contexts.ACCOUNT.ordinal()] = new AccountMenu();
+        screens[Contexts.BILL.ordinal()] = new BillCreator();
+        screens[Contexts.CHARTS.ordinal()] = new ChartsMenu();
+        screens[Contexts.GROUPS.ordinal()] = new GroupsMenu();
+        screens[Contexts.EXPENSES.ordinal()] = new ExpensesMenu();
+        screens[Contexts.LOGIN.ordinal()] = new LoginMenu();
     }
 
-    public boolean run() {
-        Box centerLayout = Box.createVerticalBox();
-        centerLayout.setBackground(BackgroundColor);
-        centerLayout.add(Box.createVerticalGlue());
-        // ? Login Menu
-        LoginMenu login = new LoginMenu();
-        centerLayout.add(login, BorderLayout.CENTER);
-        login.openScreen();
+    private void createButtons() {
+        sidePanel.add(Box.createVerticalGlue());
 
-        // ! End of Login Process
+        buttons = new JButton[Contexts.values().length];
 
-        // ? Account Menu
-        screens = new PanelScreen[buttonNames.length];
-        screens[0] = new AccountMenu();
-        screens[1] = new BillCreator();
-        screens[2] = new ChartsMenu();
-        screens[3] = new GroupsMenu();
-        screens[4] = new ExpensesMenu();
+        // Creates the buttons for each context except for the logout button
+        for (int i = 0; i < Contexts.values().length; i++) {
+            if (i == Contexts.LOGIN.ordinal())
+                continue;
 
-        for (int i = 0; i < screens.length; i++) {
-            screens[i].closeScreen();
+            buttons[i] = new MenuButton(buttonNames[i], new Color(255, 255, 255));
+
+            buttons[i].addActionListener(e -> updateContext(e));
+
+            sidePanel.add(buttons[i]);
+            sidePanel.add(Box.createVerticalStrut(25));
         }
-        centerLayout.add(Box.createVerticalGlue());
-        this.getContentPane().add(centerLayout, BorderLayout.CENTER);
 
-        // ? Side Buttons
-        Box sideButtons = Box.createVerticalBox();
-        buttons = new MenuButton[buttonNames.length];
+        // Easier manipulation of the logout button
+        buttons[Contexts.LOGIN.ordinal()] = new JButton(buttonNames[Contexts.LOGIN.ordinal()]);
+        JButton logoutButton = buttons[Contexts.LOGIN.ordinal()];
 
-        for (int i = 0; i < buttonNames.length; i++) {
-            sideButtons.add(Box.createVerticalGlue());
-            buttons[i] = new MenuButton(buttonNames[i]);
-            sideButtons.add(buttons[i]);
-
-            buttons[i].addActionListener(e -> {
-                this.clearScreen();
-                this.buttons[this.getCurContext()].closeMenu();
-                for (int j = 0; j < buttonNames.length; j++) {
-                    if (e.getSource() == buttons[j]) {
-                        this.setCurContext(j);
-                        break;
-                    }
-                }
-                centerLayout.removeAll();
-                centerLayout.add(Box.createVerticalGlue());
-                centerLayout.add(screens[this.getCurContext()], BorderLayout.CENTER);
-                centerLayout.add(Box.createVerticalGlue());
-                this.buttons[this.getCurContext()].openMenu();
-                screens[this.getCurContext()].openScreen();
-                this.refresh();
-            });
-        }
-        sideButtons.add(Box.createVerticalStrut(50));
-
-        JButton logoutButton = new JButton("Logout");
         logoutButton.setBackground(new Color(235, 235, 235));
         logoutButton.setFont(new Font("Arial", Font.BOLD, 13));
         logoutButton.setSize(50, 50);
-        logoutButton.setVisible(false);
+        logoutButton.setVisible(true);
 
-        sideButtons.add(logoutButton);
+        logoutButton.addActionListener(e -> logout(e));
 
-        this.getContentPane().add(sideButtons, BorderLayout.EAST);
+        sidePanel.add(buttons[Contexts.LOGIN.ordinal()]);
+
+        sidePanel.add(Box.createVerticalGlue());
+    }
+
+    private void updateContext(ActionEvent e) {
+        // Clears the screen for the new context
+        this.centerLayout.removeAll();
+        centerLayout.add(Box.createVerticalGlue());
+
+        // Sets the new context
+        if (e != null) {
+            for (int i = 0; i < buttons.length; i++) {
+                buttons[i].setSelected(false);
+                if (e.getSource() == buttons[i]) {
+                    this.currentContext = Contexts.values()[i];
+                    buttons[i].setSelected(true);
+                }
+            }
+        }
+
+        // Adds the new context to the screen
+        this.centerLayout.add(screens[this.currentContext.ordinal()]);
+
+        switch (this.currentContext) {
+            case LOGIN:
+                this.sidePanel.setVisible(false);
+                break;
+            default:
+                this.sidePanel.setVisible(true);
+                break;
+        }
+
+        centerLayout.add(Box.createVerticalGlue());
         this.refresh();
+    }
 
-        login.getBtnLogin().addActionListener(e -> {
-            this.setLoggedIn(true);
-            login.closeScreen();
-            centerLayout.removeAll();
-            centerLayout.add(Box.createVerticalGlue());
-            centerLayout.add(Box.createVerticalGlue());
-            this.openButtons();
-            logoutButton.setVisible(true);
-            this.refresh();
-        });
+    private void login(ActionEvent e) {
+        LoginMenu login = (LoginMenu) this.screens[Contexts.LOGIN.ordinal()];
 
-        logoutButton.addActionListener(e -> {
-            this.setLoggedIn(false);
-            login.openScreen();
-            this.clearScreen();
-            centerLayout.removeAll();
-            centerLayout.add(Box.createVerticalGlue());
-            centerLayout.add(login, BorderLayout.CENTER);
-            centerLayout.add(Box.createVerticalGlue());
-            this.closeButtons();
-            logoutButton.setVisible(false);
-            this.refresh();
-        });
+        if (login.authorizeUser()) {
+            this.currentContext = Contexts.ACCOUNT;
+            this.buttons[Contexts.ACCOUNT.ordinal()].setSelected(true);
+        } else {
+            this.currentContext = Contexts.LOGIN;
+        }
 
-        return true;
+        this.updateContext(null);
+    }
+
+    private void logout(ActionEvent e) {
+        this.currentContext = Contexts.LOGIN;
+        this.updateContext(null);
     }
 }
