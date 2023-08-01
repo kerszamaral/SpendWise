616f84e07fbae3c6ab18673a7e8196d1e452db43
Ian Kersz <kerszamaral@gmail.com>
2023-07-31 23:15:30 -0300

    Created first GUI

--- src/SpendWise/MainMenu.java
+++ src/SpendWise/MainMenu.java
@@ -1,10 +1,10 @@
 package SpendWise;
 
-import SpendWise.Graphics.LoginMenu;
+import SpendWise.Graphics.Menus.*;
+import SpendWise.Graphics.*;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-import SpendWise.Graphics.Screen;
 
 public class MainMenu extends JFrame {
     private int curContext;
@@ -12,6 +12,12 @@ public class MainMenu extends JFrame {
     private final static int ScreenWidth = 1920 / 2;
     private final static int ScreenHeight = 1080 / 2;
     private final static String AppName = "SpendWise";
+    private final static Color BackgroundColor = new Color(0, 177, 216);
+
+    private final static String[] buttonNames = { "Account", "Bill", "Charts", "Groups", "Expenses" };
+
+    private PanelScreen[] screens;
+    private MenuButton[] buttons;
 
     public MainMenu() {
         this.initialize();
@@ -23,6 +29,9 @@ public class MainMenu extends JFrame {
         this.setTitle(AppName);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().setLayout(new BorderLayout());
+        this.curContext = 0;
+        this.loggedIn = false;
+        this.getContentPane().setBackground(BackgroundColor);
     }
 
     public void setCurContext(int curContext) {
@@ -41,18 +50,111 @@ public class MainMenu extends JFrame {
         return loggedIn;
     }
 
-    private void changeContext() {
+    private void closeButtons() {
+        for (int i = 0; i < buttons.length; i++) {
+            if (buttons[i] != null && buttons[i].isVisible())
+                buttons[i].setVisible(false);
+        }
         return;
     }
 
-    private void makeBackground() {
+    private void openButtons() {
+        for (int i = 0; i < buttons.length; i++) {
+            if (buttons[i] != null && !buttons[i].isVisible())
+                buttons[i].setVisible(true);
+        }
+        return;
+    }
+
+    private void refresh() {
+        this.revalidate();
+        this.repaint();
+        return;
+    }
+
+    private void clearScreen() {
+        for (int i = 0; i < screens.length; i++) {
+            screens[i].closeScreen();
+        }
         return;
     }
 
     public boolean run() {
+        Box centerLayout = Box.createVerticalBox();
+        centerLayout.setBackground(BackgroundColor);
+        // ? Login Menu
         LoginMenu login = new LoginMenu();
+        centerLayout.add(login);
+        login.openScreen();
 
-        this.getContentPane().add(login.getPnlButtons(), BorderLayout.CENTER);
+        // ! End of Login Process
+
+        // ? Account Menu
+        screens = new PanelScreen[buttonNames.length];
+        screens[0] = new AccountMenu();
+        screens[1] = new BillCreator();
+        screens[2] = new ChartsMenu();
+        screens[3] = new GroupsMenu();
+        screens[4] = new ExpensesMenu();
+
+        for (int i = 0; i < screens.length; i++) {
+            centerLayout.add(screens[i]);
+            screens[i].closeScreen();
+        }
+        this.getContentPane().add(centerLayout, BorderLayout.CENTER);
+
+        // ? Side Buttons
+        Box sideButtons = Box.createVerticalBox();
+        buttons = new MenuButton[buttonNames.length];
+
+        for (int i = 0; i < buttonNames.length; i++) {
+            sideButtons.add(Box.createVerticalGlue());
+            buttons[i] = new MenuButton(buttonNames[i]);
+            sideButtons.add(buttons[i]);
+
+            buttons[i].addActionListener(e -> {
+                this.clearScreen();
+                this.buttons[this.getCurContext()].closeMenu();
+                for (int j = 0; j < buttonNames.length; j++) {
+                    if (e.getSource() == buttons[j]) {
+                        this.setCurContext(j);
+                        break;
+                    }
+                }
+                this.buttons[this.getCurContext()].openMenu();
+                screens[this.getCurContext()].openScreen();
+                this.refresh();
+            });
+        }
+        sideButtons.add(Box.createVerticalStrut(50));
+
+        JButton logoutButton = new JButton("Logout");
+        logoutButton.setBackground(new Color(235, 235, 235));
+        logoutButton.setFont(new Font("Arial", Font.BOLD, 13));
+        logoutButton.setSize(50, 50);
+        logoutButton.setVisible(false);
+
+        sideButtons.add(logoutButton);
+
+        this.getContentPane().add(sideButtons, BorderLayout.EAST);
+        this.refresh();
+
+        login.getBtnLogin().addActionListener(e -> {
+            this.setLoggedIn(true);
+            login.closeScreen();
+            this.openButtons();
+            logoutButton.setVisible(true);
+            this.refresh();
+        });
+
+        logoutButton.addActionListener(e -> {
+            this.setLoggedIn(false);
+            login.openScreen();
+            this.clearScreen();
+            this.closeButtons();
+            logoutButton.setVisible(false);
+            this.refresh();
+        });
 
         return true;
     }
