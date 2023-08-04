a8740ff99d21d4c459f685ea8fcf6a7748bbb100
Nathan Alonso Guimarães <nathan.guimaraes@ufrgs.br>
2023-08-04 19:39:40 -0300

    Simplified LoginLabels

--- src/SpendWise/Utils/LoginLabels.java
+++ src/SpendWise/Utils/LoginLabels.java
@@ -1,19 +1,9 @@
 package SpendWise.Utils;
 
 public enum LoginLabels {
-    NAME(0),
-    USERNAME(1),
-    EMAIL(2),
-    PASSWORD(3),
-    REPEAT_PASSWORD(4);
-
-    private final int value;
-
-    private LoginLabels(int value) {
-        this.value = value;
-    }
-
-    public int getValue() {
-        return value;
-    }
+    NAME,
+    USERNAME,
+    EMAIL,
+    PASSWORD,
+    REPEAT_PASSWORD;  
 }
