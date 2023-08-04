f4bc66b8056ec7db9eff6bb310411fcd494882ae
Nathan Alonso Guimarães <nathan.guimaraes@ufrgs.br>
2023-08-04 20:56:17 -0300

    Updates in account menu

--- src/SpendWise/Graphics/Menus/AccountMenu.java
+++ src/SpendWise/Graphics/Menus/AccountMenu.java
@@ -1,6 +1,9 @@
 package SpendWise.Graphics.Menus;
 
+
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
 
 import javax.swing.*;
 
@@ -10,10 +13,6 @@ import SpendWise.User;
 
 public class AccountMenu extends Screen {
     // TODO implement AccountMenu integration with User class
-    private JTextField txtName;
-    private JTextField txtUsername;
-    private JTextField txtEmail;
-    private JTextField txtPassword;
     private User loggedUser;
     
     public AccountMenu(User loggedUser) {
@@ -25,35 +24,49 @@ public class AccountMenu extends Screen {
     protected void initialize() {
         super.initialize();
 
-        this.setLayout(null);
-        this.setBackground(Color.green);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setBackground(BACKGROUND_COLOR);
 
-        JLabel lblName = new JLabel("Name: ");
-        lblName.setBounds(10, 10, 80, 25);
-        txtName = new JTextField(loggedUser.getName(), 20);
-        txtName.setBounds(100, 10, 160, 25);
-        this.add(lblName);
-        this.add(txtName);
-
-        JLabel lblUsername = new JLabel("Username: ");
-        lblUsername.setBounds(10, 40, 80, 25);
-        txtUsername = new JTextField("Default Username", 20);
-        txtUsername.setBounds(100, 40, 160, 25);
-        this.add(lblUsername);
-        this.add(txtUsername);
-
-        JLabel lblEmail = new JLabel("Email: ");
-        lblEmail.setBounds(10, 70, 80, 25);
-        txtEmail = new JTextField("Default Email", 20);
-        txtEmail.setBounds(100, 70, 160, 25);
-        this.add(lblEmail);
-        this.add(txtEmail);
-
-        JLabel lblPassword = new JLabel("Password: ");
-        lblPassword.setBounds(10, 100, 80, 25);
-        txtPassword = new JTextField("**********", 20);
-        txtPassword.setBounds(100, 100, 160, 25);
-        this.add(lblPassword);
-        this.add(txtPassword);
+        addTextField("Name: ", loggedUser.getName(), 100);
+        addTextField("Username: ", loggedUser.getUsername(), 100);
+        addTextField("E-mail: ", loggedUser.getEmail(), 100);
+        addPasswordField("Password: ", loggedUser.getPassword(), 100);
+        addEditButton();
     }
+
+    private void addTextField(String label, String initialValue, int width) {
+
+        JLabel lbl = new JLabel(label);
+        this.add(lbl);
+
+        JTextField textField = new JTextField(initialValue, 20);
+        textField.setEditable(false);
+        textField.setPreferredSize(new Dimension(width, textField.getPreferredSize().height));
+        this.add(textField);
+
+    }
+
+    private void addPasswordField(String label, String initialValue, int width) {
+        JLabel lbl = new JLabel(label);
+        this.add(lbl);
+        JPasswordField passwordField = new JPasswordField(initialValue, 20);
+        passwordField.setEditable(false);
+        passwordField.setPreferredSize(new Dimension(width, passwordField.getPreferredSize().height));
+        this.add(passwordField);
+
+    }
+
+    private void addEditButton() {
+        JButton btnEdit = new JButton("Edit");
+        btnEdit.setBackground(Color.BLACK);
+        btnEdit.setForeground(BACKGROUND_COLOR);
+        btnEdit.addActionListener(e -> this.edit(e));
+        this.add(btnEdit);
+    }
+
+    private void edit(ActionEvent e) {
+        // TODO implement edit button
+        // Criar um pop-up que permite editar os campos
+    }
+
 }
