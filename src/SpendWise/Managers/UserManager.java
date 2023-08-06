e1ef13761bd0b6a729c036e526024fc9c6494a3c
Nathan Alonso Guimarães <nathan.guimaraes@ufrgs.br>
2023-08-06 02:17:27 -0300

    Admin user temporarely added.

--- src/SpendWise/Managers/UserManager.java
+++ src/SpendWise/Managers/UserManager.java
@@ -10,6 +10,9 @@ public class UserManager {
     public UserManager() {
         this.users = new Hashtable<String, User>();
         this.loggedUser = new User();
+
+        // Just to skip all the sign in process
+        createUser(new User("admin", "admin", "admin", "admin" ,0, 0));
     }
     
     public User getLoggedUser() {
