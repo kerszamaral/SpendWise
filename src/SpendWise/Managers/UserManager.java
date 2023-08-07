d685738ac3fde79b579fe5c4c2d85b6f60f6d336
Ian Kersz <kerszamaral@gmail.com>
2023-08-07 16:57:40 -0300

    Fixed formatting on UserManager

--- src/SpendWise/Managers/UserManager.java
+++ src/SpendWise/Managers/UserManager.java
@@ -6,20 +6,20 @@ import SpendWise.User;
 public class UserManager {
     private Hashtable<String, User> users;
     private User loggedUser;
-    
+
     public UserManager() {
         this.users = new Hashtable<String, User>();
         this.loggedUser = new User();
 
         // Just to skip all the sign in process
-        createUser(new User("admin", "admin", "admin@admin.com", "admin" ,0, 0));
+        createUser(new User("admin", "admin", "admin@admin.com", "admin", 0, 0));
     }
-    
+
     public User getLoggedUser() {
         return this.loggedUser;
     }
 
-    public boolean createUser(User user){
+    public boolean createUser(User user) {
 
         if (this.users.containsKey(user.getUsername())) {
             return false;
@@ -27,19 +27,19 @@ public class UserManager {
         this.users.put(user.getUsername(), user);
         return true;
     }
-    
+
     public boolean validateLogin(String username, String password) {
         if (!this.users.containsKey(username)) {
             return false;
         }
         User user = this.users.get(username);
-        
+
         boolean isValid = user.checkPassword(password);
-        
-        if(isValid){
+
+        if (isValid) {
             this.loggedUser = user;
         }
-        
+
         return isValid;
     }
 }
