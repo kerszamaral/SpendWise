3e05242be5e5257e5af71d85cbefd443c92db7be
Ian Kersz <kerszamaral@gmail.com>
2023-08-08 17:57:27 -0300

    Finished first BillCreator implementation

--- src/SpendWise/Graphics/Menus/BillCreator.java
+++ src/SpendWise/Graphics/Menus/BillCreator.java
@@ -3,25 +3,20 @@ package SpendWise.Graphics.Menus;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
-import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 
-import javax.swing.BoxLayout;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JFormattedTextField;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
 
-import SpendWise.Bills.Expense;
-import SpendWise.Bills.OneTime;
+import SpendWise.Bills.*;
 import SpendWise.Graphics.Screen;
 import SpendWise.Managers.ExpensesManager;
 import SpendWise.Utils.GraphicsUtils;
 import SpendWise.Utils.Offsets;
+import SpendWise.Utils.Enums.BillsFields;
 import SpendWise.Utils.Enums.ExpenseType;
 import SpendWise.Utils.Enums.PanelOrder;
 
@@ -29,14 +24,15 @@ public class BillCreator extends Screen {
     private ExpensesManager expensesManager;
     private JPanel pnlCentral;
 
-    private JFormattedTextField valueField;
-    private JCheckBox essentialCheckBox;
-    private JFormattedTextField dateField;
-    private JTextField descriptionField;
-    private JComboBox<ExpenseType> typeSelector;
-    private JPanel pnlTypeSpecific;
+    private JComponent[] fields;
+    private JComboBox<String> typeSelector;
+    final private SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
+    final private Dimension fieldSize = new Dimension(100, 30);
 
-    private JCheckBox onetimeCheckBox;
+    JPanel pnlTypeSpecific;
+
+    private JCheckBox oneTimeCheckBox;
+    private JFormattedTextField recurringEndDateField;
 
     public BillCreator(ExpensesManager expensesManager) {
         this.expensesManager = expensesManager;
@@ -45,134 +41,137 @@ public class BillCreator extends Screen {
 
     @Override
     protected void initialize() {
-        blankPanels = GraphicsUtils.initializeOffsets(this, DEFAULT_OFFSETS);
+        Offsets offsets = new Offsets(50, 50, 50, 50);
+        blankPanels = GraphicsUtils.createPanelWithCenter(this, offsets, ACCENT_COLOR);
+
+        blankPanels[PanelOrder.NORTH.ordinal()].add(new JLabel("Create a new bill"));
+        super.getBlankPanel(PanelOrder.NORTH).setSize(new Dimension(0, 0));
 
         pnlCentral = super.getBlankPanel(PanelOrder.CENTRAL);
-        pnlCentral.setBackground(BACKGROUND_COLOR);
-        pnlCentral.setLayout(new GridLayout(8, 2));
+        pnlCentral.setLayout(new GridLayout(BillsFields.values().length * 2 + 1, 1));
 
-        this.createValueField();
-        this.createEssentialButton();
-        this.createDateSelector();
-        this.createDescriptionField();
-        this.createTypeSelector();
+
+
+        fields = new JComponent[BillsFields.values().length];
+
+        for (BillsFields field : BillsFields.values()) {
+            String label = field.getLabel() + ": ";
+            JLabel lbl = new JLabel(label);
+            pnlCentral.add(lbl);
+
+            JComponent fieldType = null;
+            switch (field) {
+                case VALUE:
+                    JFormattedTextField valueField = new JFormattedTextField(NumberFormat.getCurrencyInstance());
+                    valueField.setValue(Double.valueOf(0.0));
+                    valueField.setColumns(10);
+                    fieldType = valueField;
+                    break;
+
+                case ESSENTIAL:
+                    JCheckBox essentialCheckBox = new JCheckBox();
+                    fieldType = essentialCheckBox;
+                    break;
+
+                case DATE:
+                    JFormattedTextField dateField = new JFormattedTextField(dateFormatter);
+                    dateField.setValue(new java.util.Date());
+                    fieldType = dateField;
+                    break;
+
+                case DESCRIPTION:
+                    JTextField descriptionField = new JTextField();
+                    fieldType = descriptionField;
+                    break;
+
+                case TYPE:
+                    String[] types = new String[ExpenseType.values().length];
+                    for (ExpenseType type : ExpenseType.values()) {
+                        types[type.ordinal()] = type.getType();
+                    }
+                    typeSelector = new JComboBox<String>(types);
+                    typeSelector.addActionListener(e -> createTypeSpecificFields(e));
+                    fieldType = typeSelector;
+                    break;
+
+                default:
+                    fieldType = new JTextField();
+                    break;
+            }
+
+            fieldType.setMinimumSize(fieldSize);
+            fieldType.setPreferredSize(fieldSize);
+            fieldType.setMaximumSize(fieldSize);
+
+            fieldType.setEnabled(true);
+            fieldType.setBackground(ACCENT_COLOR);
+
+            fields[field.ordinal()] = fieldType;
+            pnlCentral.add(fieldType);
+        }
 
         pnlTypeSpecific = new JPanel();
-        pnlTypeSpecific.setBackground(BACKGROUND_COLOR);
+        pnlTypeSpecific.setBackground(ACCENT_COLOR);
         pnlCentral.add(pnlTypeSpecific);
 
-        this.createTypeSpecificFields(null);
-        Offsets offsets = new Offsets(10, 10, 400, 20);
-        GraphicsUtils.createButton(this.getBlankPanel(PanelOrder.SOUTH), offsets, "Submit", BACKGROUND_COLOR,
+        Offsets offsetsBtn = new Offsets(10, 10, 400, 20);
+        GraphicsUtils.createButton(this.getBlankPanel(PanelOrder.SOUTH), offsetsBtn,
+                "Submit", ACCENT_COLOR,
                 e -> submit(e));
     }
 
-    private void createValueField() {
-        JLabel lbl = new JLabel("Value:");
-        pnlCentral.add(lbl);
-
-        valueField = new JFormattedTextField(NumberFormat.getCurrencyInstance());
-        valueField.setValue(Double.valueOf(0.0));
-        valueField.setColumns(10);
-        valueField.setPreferredSize(new Dimension(100, valueField.getPreferredSize().height));
-        pnlCentral.add(valueField);
-    }
-
-    private void createEssentialButton() {
-        JLabel lbl = new JLabel("Essential:");
-        pnlCentral.add(lbl);
-
-        essentialCheckBox = new JCheckBox();
-        essentialCheckBox.setBackground(BACKGROUND_COLOR);
-        essentialCheckBox.setPreferredSize(new Dimension(100, essentialCheckBox.getPreferredSize().height));
-        pnlCentral.add(essentialCheckBox);
-    }
-
-    private void createDateSelector() {
-        JLabel lbl = new JLabel("Date:");
-        pnlCentral.add(lbl);
-
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        dateField = new JFormattedTextField(dateFormat);
-        dateField.setValue(new java.util.Date());
-        dateField.setColumns(10);
-        dateField.setPreferredSize(new Dimension(100, dateField.getPreferredSize().height));
-        pnlCentral.add(dateField);
-    }
-
-    private void createDescriptionField() {
-        JLabel lbl = new JLabel("Description:");
-        pnlCentral.add(lbl);
-
-        descriptionField = new JTextField();
-        descriptionField.setColumns(10);
-        descriptionField.setPreferredSize(new Dimension(100, descriptionField.getPreferredSize().height));
-        pnlCentral.add(descriptionField);
-    }
-
-    private void createTypeSelector() {
-        JLabel lbl = new JLabel("Type:");
-        pnlCentral.add(lbl);
-
-        typeSelector = new JComboBox<ExpenseType>(ExpenseType.values());
-        typeSelector.setPreferredSize(new Dimension(100, typeSelector.getPreferredSize().height));
-        typeSelector.setSelectedIndex(0);
-        typeSelector.addActionListener(e -> createTypeSpecificFields(e));
-        pnlCentral.add(typeSelector);
-    }
-
     private void createTypeSpecificFields(ActionEvent e) {
-        ExpenseType type = (ExpenseType) typeSelector.getSelectedItem();
+        ExpenseType type = ExpenseType.values()[typeSelector.getSelectedIndex()];
+
         pnlTypeSpecific.removeAll();
+        pnlTypeSpecific.setLayout(new BoxLayout(pnlTypeSpecific, BoxLayout.X_AXIS));
+        JLabel lbl;
+
         switch (type) {
             case FIXED:
-                this.createFixedFields();
+                // Has no new fields;
                 break;
+
             case ONETIME:
-                this.createOnetimeFields();
+                lbl = new JLabel("Has been paid?");
+                lbl.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
+                pnlTypeSpecific.add(lbl);
+
+                oneTimeCheckBox = new JCheckBox();
+                oneTimeCheckBox.setBackground(ACCENT_COLOR);
+                pnlTypeSpecific.add(oneTimeCheckBox);
                 break;
+
             case RECURRING:
-                this.createRecurringFields();
+                lbl = new JLabel("End date: ");
+                lbl.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
+                pnlTypeSpecific.add(lbl);
+
+                recurringEndDateField = new JFormattedTextField(dateFormatter);
+                recurringEndDateField.setValue(new java.util.Date());
+
+                recurringEndDateField.setMinimumSize(fieldSize);
+                recurringEndDateField.setPreferredSize(fieldSize);
+                recurringEndDateField.setMaximumSize(fieldSize);
+
+                recurringEndDateField.setEnabled(true);
+                recurringEndDateField.setBackground(ACCENT_COLOR);
+
+                pnlTypeSpecific.add(recurringEndDateField);
                 break;
+
             default:
                 break;
         }
+
         this.refresh();
     }
 
-    private void createFixedFields() {
-        JLabel lbl = new JLabel("Fixed:");
-        pnlTypeSpecific.add(lbl);
-
-        JCheckBox fixedCheckBox = new JCheckBox();
-        fixedCheckBox.setBackground(BACKGROUND_COLOR);
-        pnlTypeSpecific.add(fixedCheckBox);
-    }
-
-    private void createOnetimeFields() {
-        pnlTypeSpecific.setLayout(new BoxLayout(pnlTypeSpecific, BoxLayout.X_AXIS));
-        JLabel lbl = new JLabel("Has been paid?");
-        lbl.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
-        pnlTypeSpecific.add(lbl);
-
-        onetimeCheckBox = new JCheckBox();
-        onetimeCheckBox.setBackground(BACKGROUND_COLOR);
-        pnlTypeSpecific.add(onetimeCheckBox);
-    }
-
-    private void createRecurringFields() {
-        JLabel lbl = new JLabel("Recurring:");
-        pnlTypeSpecific.add(lbl);
-
-        JCheckBox recurringCheckBox = new JCheckBox();
-        recurringCheckBox.setBackground(BACKGROUND_COLOR);
-        pnlTypeSpecific.add(recurringCheckBox);
-    }
-
     private void submit(ActionEvent action) {
-        ExpenseType type = (ExpenseType) typeSelector.getSelectedItem();
+        ExpenseType type = ExpenseType.values()[typeSelector.getSelectedIndex()];
         Expense exp = null;
 
+        JFormattedTextField valueField = (JFormattedTextField) fields[BillsFields.VALUE.ordinal()];
         double value = 0;
         try {
             value = NumberFormat.getCurrencyInstance().parse(valueField.getText()).doubleValue();
@@ -180,27 +179,38 @@ public class BillCreator extends Screen {
             return;
         }
 
+        JCheckBox essentialCheckBox = (JCheckBox) fields[BillsFields.ESSENTIAL.ordinal()];
         boolean essential = essentialCheckBox.isSelected();
 
+        JFormattedTextField dateField = (JFormattedTextField) fields[BillsFields.DATE.ordinal()];
         LocalDate date = null;
         try {
-            date = LocalDate.parse(dateField.getText());
+            Date temp = dateFormatter.parse(dateField.getText());
+            date = temp.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         } catch (Exception e) {
             return;
         }
 
+        JTextField descriptionField = (JTextField) fields[BillsFields.DESCRIPTION.ordinal()];
         String description = descriptionField.getText();
 
         switch (type) {
             case FIXED:
-                this.createFixedFields();
+                exp = new Fixed(value, essential, date, description);
                 break;
             case ONETIME:
-                boolean paid = onetimeCheckBox.isSelected();
+                boolean paid = oneTimeCheckBox.isSelected();
                 exp = new OneTime(value, essential, date, description, paid);
                 break;
             case RECURRING:
-                this.createRecurringFields();
+                LocalDate endDate = null;
+                try {
+                    Date temp = dateFormatter.parse(recurringEndDateField.getText());
+                    endDate = temp.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+                } catch (Exception e) {
+                    return;
+                }
+                exp = new Recurring(value, essential, date, description, endDate);
                 break;
             default:
                 break;
@@ -208,6 +218,10 @@ public class BillCreator extends Screen {
 
         if (exp != null) {
             expensesManager.addExpense(exp);
+        }
+
+        for (Expense e : expensesManager.getExpenses()) {
+            System.out.println(e.toString());
         }
     }
 }
