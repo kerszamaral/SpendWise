0a45a64ae795b754bb1d0e09f8729895cdec614e
Ian Kersz <kerszamaral@gmail.com>
2023-08-09 15:42:41 -0300

    Moved Default Sizes

---
+++ src/SpendWise/Utils/Graphics/Sizes.java
@@ -0,0 +1,11 @@
+package SpendWise.Utils.Graphics;
+
+import java.awt.Dimension;
+
+import SpendWise.Utils.Offsets;
+
+public interface Sizes {
+    public final Dimension DEFAULT_FIELD_SIZE = new Dimension(100, 30);
+    public final Offsets DEFAULT_OFFSETS = new Offsets(50, 50, 100, 0);
+
+}
