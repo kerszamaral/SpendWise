848f367292b04bea882956ddcabc33458caa7375
Ian Kersz <kerszamaral@gmail.com>
2023-08-09 15:42:21 -0300

    Moved Fonts

---
+++ src/SpendWise/Utils/Graphics/Fonts.java
@@ -0,0 +1,9 @@
+package SpendWise.Utils.Graphics;
+
+import java.awt.Font;
+
+public interface Fonts {
+    public final Font STD_FONT = new Font("Arial", Font.PLAIN, 14);
+    public final Font STD_FONT_BOLD = new Font("Arial", Font.BOLD, 14);
+
+}
