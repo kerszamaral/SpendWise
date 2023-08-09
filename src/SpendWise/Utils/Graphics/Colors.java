1763a4a9948c114134947c2128680ac13036a99d
Ian Kersz <kerszamaral@gmail.com>
2023-08-09 15:42:16 -0300

    Moved Colors

--- src/SpendWise/Utils/Graphics/Colors.java
+++ src/SpendWise/Utils/Graphics/Colors.java
@@ -3,7 +3,7 @@ package SpendWise.Utils.Graphics;
 import java.awt.Color;
 
 public interface Colors {
-    Color BACKGROUND_COLOR = new Color(98, 210, 162);
-    Color ACCENT_COLOR = new Color(255, 255, 255);
+    public final Color BACKGROUND_COLOR = new Color(98, 210, 162);
+    public final Color ACCENT_COLOR = new Color(255, 255, 255);
 
 }
