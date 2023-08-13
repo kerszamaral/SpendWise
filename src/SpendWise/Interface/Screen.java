a23feaa05328204a7e76c521bba32f7f5f5074e2
Ian Kersz <kerszamaral@gmail.com>
2023-08-13 12:06:21 -0300

    Added override and changed refresh location

--- src/SpendWise/Interface/Screen.java
+++ src/SpendWise/Interface/Screen.java
@@ -2,25 +2,27 @@ package SpendWise.Interface;
 
 import javax.swing.JPanel;
 
+import SpendWise.Utils.Paths;
 import SpendWise.Utils.Enums.PanelOrder;
 import SpendWise.Utils.Graphics.BlankPanels;
 import SpendWise.Utils.Graphics.Colors;
+import SpendWise.Utils.Graphics.Components;
 import SpendWise.Utils.Graphics.Fonts;
 import SpendWise.Utils.Graphics.Icons;
-import SpendWise.Utils.Graphics.Misc;
 import SpendWise.Utils.Graphics.Refresh;
 import SpendWise.Utils.Graphics.Sizes;
 
-public abstract class Screen extends JPanel implements BlankPanels, Colors, Fonts, Sizes, Refresh, Icons {
+public abstract class Screen extends JPanel implements BlankPanels, Colors, Fonts, Sizes, Refresh, Icons, Paths {
     protected JPanel[] blankPanels;
 
     protected abstract void initialize();
 
     @Override
     public void refresh() {
-        Misc.refresh(this);
+        Components.refresh(this);
     }
 
+    @Override
     public JPanel getBlankPanel(PanelOrder panel) {
         return blankPanels[panel.ordinal()];
     }
