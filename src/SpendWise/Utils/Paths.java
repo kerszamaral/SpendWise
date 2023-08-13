527622d68994b25a9cfecc8399cc36290d5c1454
Ian Kersz <kerszamaral@gmail.com>
2023-08-13 11:56:30 -0300

    added paths

---
+++ src/SpendWise/Utils/Paths.java
@@ -0,0 +1,9 @@
+package SpendWise.Utils;
+
+public interface Paths {
+    public static final String RESOURCES_PATH = "res/";
+    public static final String IMAGES_PATH = RESOURCES_PATH + "Images/";
+    public static final String IMAGES_EXT = ".png";
+    public static final String ICONS_PATH = IMAGES_PATH + "Icons/";
+    public static final String FONTS_PATH = RESOURCES_PATH + "Fonts/";
+}
