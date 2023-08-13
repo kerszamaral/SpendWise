bcf979fb32e8de86164da6071c5cb8bd991901d6
Ian Kersz <kerszamaral@gmail.com>
2023-08-13 18:36:21 -0300

    Removed wrong Expense Managers tests

--- src/Tests/Managers/ExpensesManagerTest.java
+++ src/Tests/Managers/ExpensesManagerTest.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import SpendWise.Logic.Bills.Expense;
 import SpendWise.Logic.Bills.Fixed;
 import SpendWise.Logic.Managers.ExpensesManager;
-import SpendWise.Utils.Enums.ExpenseType;
 
 public class ExpensesManagerTest {
     private ExpensesManager expensesManager;
@@ -28,60 +27,5 @@ public class ExpensesManagerTest {
         expensesManager.addExpense(exp);
         assertEquals(1, expensesManager.getExpenses().size());
     }
-
-    @Test
-    public void testCreateFixedExpense() {
-        expensesManager.createFixedExpense(100, true, LocalDate.now(), "test");
-        assertEquals(1, expensesManager.getExpenses().size());
-    }
-
-    @Test
-    public void testCreateOneTimeExpense() {
-        expensesManager.createOneTimeExpense(100, true, LocalDate.now(), "test", false);
-        assertEquals(1, expensesManager.getExpenses().size());
-    }
-
-    @Test
-    public void testCreateRecurringExpense() {
-        expensesManager.createRecurringExpense(100, true, LocalDate.now(), "test", LocalDate.now());
-        assertEquals(1, expensesManager.getExpenses().size());
-    }
-
-    @Test
-    public void testCreateExpense() {
-        expensesManager.createExpense(100, true, LocalDate.now(), "test", ExpenseType.ONETIME, false, null);
-        assertEquals(1, expensesManager.getExpenses().size());
-    }
-
-    @Test
-    public void testRemoveExpense() {
-        expensesManager.createExpense(100, true, LocalDate.now(), "test", ExpenseType.ONETIME, false, null);
-        expensesManager.removeExpense(expensesManager.getExpenses().iterator().next());
-        assertEquals(0, expensesManager.getExpenses().size());
-    }
-
-    @Test
-    public void testGetExpenses() {
-        expensesManager.createExpense(100, true, LocalDate.now(), "test", ExpenseType.ONETIME, false, null);
-        assertEquals(1, expensesManager.getExpenses().size());
-    }
-
-    @Test
-    public void testCalculateTotalExpenses() {
-        expensesManager.createExpense(100, true, LocalDate.now(), "test", ExpenseType.ONETIME, false, null);
-        assertEquals(100, expensesManager.calculateTotalExpense(), 0.01);
-    }
-
-    @Test
-    public void testCalculateNonEssentialExpenses() {
-        expensesManager.createExpense(100, false, LocalDate.now(), "test", ExpenseType.ONETIME, false, null);
-        assertEquals(100, expensesManager.calculateNonEssentialExpenses(), 0.01);
-    }
-
-    @Test
-    public void testCalculateEssentialExpenses() {
-        expensesManager.createExpense(100, true, LocalDate.now(), "test", ExpenseType.ONETIME, false, null);
-        assertEquals(100, expensesManager.calculateEssentialExpenses(), 0.01);
-    }
-
+    // TODO finish the tests
 }
