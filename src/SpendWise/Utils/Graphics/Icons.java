287cd324fcf404c24dbde7c7ab472ec2f97dbceb
Ian Kersz <kerszamaral@gmail.com>
2023-08-13 19:24:04 -0300

    Switched logo to new path style

--- src/SpendWise/Utils/Graphics/Icons.java
+++ src/SpendWise/Utils/Graphics/Icons.java
@@ -2,7 +2,10 @@ package SpendWise.Utils.Graphics;
 
 import javax.swing.ImageIcon;
 
+import SpendWise.Utils.Paths;
+
 public interface Icons {
-    public final ImageIcon APP_LOGO = Images.createResizeAndRecolorIcon("res/Images/logo.png", 30, 30,
+    public final ImageIcon APP_LOGO = Images.createResizeAndRecolorIcon(
+            Paths.IMAGES_PATH + "logo" + Paths.IMAGES_EXT, 30, 30,
             Colors.BACKGROUND_COLOR, true);
 }
