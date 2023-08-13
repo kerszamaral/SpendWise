c7fddede11d054e7caa96e4d5082a69918db0ddd
Ian Kersz <kerszamaral@gmail.com>
2023-08-13 19:23:43 -0300

    Added fonts paths

--- src/SpendWise/Utils/Paths.java
+++ src/SpendWise/Utils/Paths.java
@@ -1,10 +1,12 @@
 package SpendWise.Utils;
 
 public interface Paths {
-    public static final String RESOURCES_PATH = "res/";
+    public static final String RESOURCES_PATH = "/SpendWise/Resources/";
     public static final String IMAGES_PATH = RESOURCES_PATH + "Images/";
     public static final String IMAGES_EXT = ".png";
     public static final String ICONS_PATH = IMAGES_PATH + "Icons/";
     public static final String FONTS_PATH = RESOURCES_PATH + "Fonts/";
+    public static final String FONTS_EXT = ".ttf";
     public static final String DATABASE_PATH = "";
+    public final String LOGO_FONT_PATH = Paths.FONTS_PATH + "Poppins-SemiBold" + Paths.FONTS_EXT;
 }
