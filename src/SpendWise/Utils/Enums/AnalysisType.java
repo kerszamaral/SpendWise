b192182417148b0dea9170d26528e0e0c0a688fd
Ian Kersz <kerszamaral@gmail.com>
2023-08-13 18:20:56 -0300

    Created Analysis Type enum

---
+++ src/SpendWise/Utils/Enums/AnalysisType.java
@@ -0,0 +1,23 @@
+package SpendWise.Utils.Enums;
+
+public enum AnalysisType {
+    USER("User"),
+    GROUP("Group"),
+    MONTH("Monthly"),
+    YEAR("Yearly");
+
+    private final String name;
+
+    private AnalysisType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
